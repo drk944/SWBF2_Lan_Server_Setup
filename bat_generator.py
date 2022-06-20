ec2632d38d965193d4b1c6be40eb83f90a098932
laser14 <derek.r.benham@gmail.com>
2022-06-20 14:55:04 -0600

    updated comments

--- bat_generator.py
+++ bat_generator.py
@@ -3,12 +3,16 @@
 
 # Instructions:
 # 1. Choose settings using the server manager, click 'START' to generate the settings file (DSManager.ini)
-# 2. Run this script. It will generate a new .bat file with those settings in the current directory
-# 3. Run the bat file and a simple server version of the game will pop up, you can then launch a new instance
-# of the game and join from there.
-
+# 2. Run this script. It will generate a new .bat file with those settings in the parent directory
+# 3. Launch the bat file from the same directory as the game* and a simple server version of the game will pop up
+# 4. To play, launch a new instance** of the game and join from there.
+    
 # *Note: The bat file needs to be in the same directory as the cracked SWBF2. I've tested it, it doesn't
-# work with the original CD version, nor the official Steam or GOG games, it needs to be the cracked one.
+    # work with the original CD version, nor the official Steam or GOG games, it needs to be the cracked one.
+
+# **Note: If you are launching the cracked version, create a copy in another directory and launch from there
+    # If not, after the game is over and you want to launch a new game, you'd need to exit out of the cracked version
+    # To reiterate, launch a Steam or GOG version, or a cracked version from a different directory to play
 
 ### Known issues ###
 # Units: The argument -u for units is a blanket statement and each game in the queue will have the
@@ -16,12 +20,12 @@
 
 # hrrespawn: I couldn't deduce what this setting does nor how it's changed. Currently hard coded to 2
 
-# Default directory: Implementation is needed to move the bat file to a different directory.
+# Default directory: Implementation is setup that the bat file is saved in the above directory, flags can
+        # be added to change that.
 import configparser
 import argparse
 
 parser = argparse.ArgumentParser()
-
 
 parser.add_argument(
     "-u", "--units", help="Number of units on a side (default 200)", nargs='?')
@@ -51,7 +55,7 @@ c = config['SESSION1']
 cl_basic = '/win /nosound /autonet dedicated /resolution 640 480'
 
 exec = c['APP_FILENAME'] + ' ' + cl_basic + ' /gamename ' + c['SESSION_NAME'] + ' /tps ' + str(tps) + ' /playerlimit ' + c['MAX_PLAYERS'] \
-    + ' /playercount ' + c['START_PLAYERS'] + ' /bots ' + c['AI_UNITS'] + '/sideselect' + ' /difficulty ' + str(int(c['AI_DIFFICULTY'])+1) \
+    + ' /playercount ' + c['START_PLAYERS'] + ' /bots ' + c['AI_UNITS'] + '  /sideselect' + ' /difficulty ' + str(int(c['AI_DIFFICULTY'])+1) \
     + ' /heroes ' + c['HEROES'] + ' /hrunlock ' + c['HERO_UNLOCK_RULE'] + ' /hrplayer ' + c['HERO_ASSIGN_RULE'] \
     + ' /hrteam ' + c['HERO_UNLOCK_TEAM'] + ' /hrrespawn 2' + ' /hrrespawnvalue ' + c['HERO_RESPAWN_TIMER'] \
     + ' /pregametime ' + c['WARMUP_TIMER'] + \
